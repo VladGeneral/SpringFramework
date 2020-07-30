9ce78d18817758c6f9c87c9c1b16da915163495a
Vladislav M <vladgeneralbox@gmail.com>
2020-07-30 21:16:31 +0300

    L6: Dependency Injection with setter/ homework

--- spring-app/src/main/java/ru/springframework/MusicPlayer.java
+++ spring-app/src/main/java/ru/springframework/MusicPlayer.java
@@ -1,13 +1,47 @@
 package ru.springframework;
 
-public class MusicPlayer {
-    private Music music;
+import java.util.ArrayList;
+import java.util.List;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+public class MusicPlayer {
+//    private Music music;
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public MusicPlayer() {
+    }
+
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
+
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
+
     public void playMusic(){
-        System.out.println("Playing " + music.getSong());
+//        System.out.println("Playing " + music.getSong());
+        musicList.forEach(x -> System.out.println(x.getSong()));
     }
 }
