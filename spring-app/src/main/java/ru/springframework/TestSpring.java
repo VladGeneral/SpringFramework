a8bb3cca5f130278d547260af8e41cb8841534c1
Vladislav M <vladgeneralbox@gmail.com>
2020-07-30 20:29:35 +0300

    L5: Dependency Injection

--- spring-app/src/main/java/ru/springframework/TestSpring.java
+++ spring-app/src/main/java/ru/springframework/TestSpring.java
@@ -5,12 +5,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContex.xml"
+                "applicationContext.xml"
         );
 
-        Music music = context.getBean("musicBean", Music.class);
+//        Music music = context.getBean("musicBean", Music.class);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
 
 
