3bb17db3c021c9ac56cc1f5213629f3ba9416eda
Vladislav M <vladgeneralbox@gmail.com>
2020-07-30 22:17:38 +0300

    L7: init/destroy scope, fabric method

--- spring-app/src/main/java/ru/springframework/ClassicalMusic.java
+++ spring-app/src/main/java/ru/springframework/ClassicalMusic.java
@@ -1,6 +1,23 @@
 package ru.springframework;
 
 public class ClassicalMusic implements Music{
+    private ClassicalMusic(){
+    }
+
+    //при scope Singleton фабричный метод вызывается только один раз
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+    public void doMyInit(){
+        System.out.println("Initialization");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Destruction");
+    }
+
+
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
--- spring-app/src/main/java/ru/springframework/TestSpring.java
+++ spring-app/src/main/java/ru/springframework/TestSpring.java
@@ -9,21 +9,26 @@ public class TestSpring {
         );
 
 //        Music music = context.getBean("musicBean", Music.class);
-
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        boolean comp = firstMusicPlayer == secondMusicPlayer;
-        System.out.println(comp);
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
+        /*init destroy scopes
+        * scope prototype не вызывает destroy метод*/
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+        /*/init destroy scopes*/
 
-        firstMusicPlayer.setVolume(10);
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
-
-        firstMusicPlayer.playMusic();
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        boolean comp = firstMusicPlayer == secondMusicPlayer;
+//        System.out.println(comp);
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        firstMusicPlayer.setVolume(10);
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
+//
+//        firstMusicPlayer.playMusic();
 
         context.close();
     }
