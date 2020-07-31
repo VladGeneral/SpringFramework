d9db6acd6936cfe05a17247949a84eee475d76ef
Vladislav M <vladgeneralbox@gmail.com>
2020-07-31 17:54:34 +0300

    delete spring-app

--- spring-app/src/main/java/ru/springframework/JazzMusic.java
+++
@@ -1,8 +0,0 @@
-package ru.springframework;
-
-public class JazzMusic implements Music{
-    @Override
-    public String getSong() {
-        return "Blue Nights";
-    }
-}
--- spring-app/src/main/java/ru/springframework/Music.java
+++
@@ -1,5 +0,0 @@
-package ru.springframework;
-
-public interface Music {
-    String getSong();
-}
