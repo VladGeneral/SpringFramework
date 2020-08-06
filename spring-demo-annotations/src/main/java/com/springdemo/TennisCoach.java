053f7f5ac284b768a797acb89e6a3207ad8b3e30
Vladislav M <vladgeneralbox@gmail.com>
2020-08-07 00:52:43 +0300

    Section 8 Java annotations - Constructor injection

--- spring-demo-annotations/src/main/java/com/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/main/java/com/springdemo/TennisCoach.java
@@ -1,12 +1,24 @@
 package com.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 //default tennisCoach
 @Component
 public class TennisCoach implements Coach {
+    private FortuneService fortuneService;
+
+    @Autowired
+    public TennisCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
 
     @Override
     public String getDailyWorkout() {
         return "Practice you backhand volley";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
     }
 }
