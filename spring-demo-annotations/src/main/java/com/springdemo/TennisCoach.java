55c8693a78f0c494552c55317bc16cd8d3914b44
Vladislav M <vladgeneralbox@gmail.com>
2020-08-07 16:00:19 +0300

    Section 9 Java annotations - Bean Lifecycle

--- spring-demo-annotations/src/main/java/com/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/main/java/com/springdemo/TennisCoach.java
@@ -2,25 +2,39 @@ package com.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 //default tennisCoach
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
+
     @Autowired
     @Qualifier("randomFortuneService")
     private FortuneService fortuneService;
+
+    public TennisCoach() {
+        System.out.println(">> TennisCoach inside default constructor");
+    }
+
+    //define my init method
+    @PostConstruct
+    public void doInit(){
+        System.out.println(">> TennisCoach: inside of doInit()");
+    }
+    //define my destroy method
+    @PreDestroy
+    public void doDestroy(){
+        System.out.println(">> TennisCoach: inside of doDestroy()");
+    }
 
     //if we will remove annotation, spring still work. as of spring 4.3 if we have one constructor autowired no longer needed
     /*@Autowired
     public TennisCoach(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
     }*/
-
-    public TennisCoach() {
-        System.out.println(">> TennisCoach inside default constructor");
-    }
 
 
     //define a setter method
