e77a81f3ad627c14dd9a9c72c7e4c8dedf852714
Vladislav M <vladgeneralbox@gmail.com>
2020-08-17 17:02:06 +0300

    Section 26 @OneToMany Uni-directional / update Course class/ Configure Fetch type

--- hb-04-one-to-many-uni/src/main/java/com/hibernate/demo/entity/Course.java
+++ hb-04-one-to-many-uni/src/main/java/com/hibernate/demo/entity/Course.java
@@ -1,6 +1,8 @@
 package com.hibernate.demo.entity;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "course")
@@ -20,6 +22,10 @@ public class Course {
             CascadeType.REFRESH})
     @JoinColumn(name = "instructor_id")
     private Instructor instructor;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private List<Review> reviews;
 
     public Course() {
     }
@@ -50,6 +56,25 @@ public class Course {
 
     public void setInstructor(Instructor instructor) {
         this.instructor = instructor;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    //add convenience methods for bi-direct relationship
+
+    public void addReview(Review review) {
+        if (reviews == null) {
+            System.out.println("reviews == null");
+            reviews = new ArrayList<>();
+        } else {
+            reviews.add(review);
+        }
     }
 
     @Override
