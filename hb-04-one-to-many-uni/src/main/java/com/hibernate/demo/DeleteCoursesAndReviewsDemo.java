379c996bf984528872aad0f8cb838046aef78f4d
Vladislav M <vladgeneralbox@gmail.com>
2020-08-18 17:10:22 +0300

    Section 26 @OneToMany Uni-directional / delete reviews

---
+++ hb-04-one-to-many-uni/src/main/java/com/hibernate/demo/DeleteCoursesAndReviewsDemo.java
@@ -0,0 +1,45 @@
+package com.hibernate.demo;
+
+import com.hibernate.demo.entity.Course;
+import com.hibernate.demo.entity.Instructor;
+import com.hibernate.demo.entity.InstructorDetail;
+import com.hibernate.demo.entity.Review;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteCoursesAndReviewsDemo {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .buildSessionFactory();
+
+        try (Session session = factory.getCurrentSession()) {
+
+            //start a transaction
+            session.beginTransaction();
+
+            //get the course
+            Course course = session.get(Course.class, 12);
+            //print the course
+            System.out.println(course);
+            //print the course reviews
+            System.out.println(course.getReviews());
+
+            //delete the course
+            session.delete(course);
+
+            //commit
+            session.getTransaction().commit();
+
+            System.out.println("Done");
+
+        } finally {
+            factory.close();
+        }
+    }
+}
