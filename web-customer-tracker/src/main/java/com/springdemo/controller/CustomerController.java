00c8a03ea060d4559d530abb4a1f8e754f7163ea
Vladislav M <vladgeneralbox@gmail.com>
2020-08-21 18:21:17 +0300

    Section 29 injecting DAO into controller

--- web-customer-tracker/src/main/java/com/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/main/java/com/springdemo/controller/CustomerController.java
@@ -1,15 +1,30 @@
 package com.springdemo.controller;
 
+import com.springdemo.dao.CustomerDAO;
+import com.springdemo.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+    //need to inject the DAO
+    @Autowired
+    private CustomerDAO customerDAO;
+
     @RequestMapping("/list")
     public String listCustomers(Model model){
+
+        //get customers from the dao
+        List<Customer> customers = customerDAO.getCustomers();
+
+        //add the customers to the model
+        model.addAttribute("customers", customers);
         return "list-customers";
     }
 
