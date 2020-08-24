d31484f211e288f88f6791e9a184b3a285fbfeab
Vladislav M <vladgeneralbox@gmail.com>
2020-08-24 14:52:54 +0300

    Section 35 AOP pointcut expressions(only AccountDAO method)

--- spring-demo-aop/src/main/java/com/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/main/java/com/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -11,7 +11,7 @@ public class MyDemoLoggingAspect {
     //this is where we add all of our related advices for logging
 
     //lets start with an @Before advice
-    @Before("execution(public void addAccount())")
+    @Before("execution(public void com.aopdemo.dao.AccountDAO.addAccount())")
     public void beforeAddAccountAdvice(){
         System.out.println("\n=======>>> Execute @Before advice on addAccount()");
     }
