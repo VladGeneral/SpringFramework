9fe25380f730b306c068fd34fbb6f79e8e794f37
Vladislav M <vladgeneralbox@gmail.com>
2020-08-26 22:32:11 +0300

    Section 45: AOP add @Pointcut and @Before

--- web-customer-tracker-aop/src/main/java/com/springdemo/aspect/CRMLoggingAspect.java
+++ web-customer-tracker-aop/src/main/java/com/springdemo/aspect/CRMLoggingAspect.java
@@ -1,6 +1,9 @@
 package com.springdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
@@ -13,8 +16,34 @@ public class CRMLoggingAspect {
     private Logger logger = Logger.getLogger(getClass().getName());
 
     //setup pointcuts
+    @Pointcut("execution(* com.springdemo.controller.*.*(..))")
+    private void forControllerPackage(){
+
+    }
+
+    @Pointcut("execution(* com.springdemo.service.*.*(..))")
+    private void forServicePackage(){
+
+    }
+
+    @Pointcut("execution(* com.springdemo.dao.*.*(..))")
+    private void forDaoPackage(){
+
+    }
+
+    @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+    private void forAppFlow(){
+
+    }
 
     //add @Before
+    @Before("forAppFlow()")
+    public void before(JoinPoint joinPoint){
+        //display method
+        logger.info("\n=====>>> in @Before: calling method: " + joinPoint.getSignature().toShortString());
+
+        //display the arguments
+    }
 
     //add @AfterReturning advice
 }
