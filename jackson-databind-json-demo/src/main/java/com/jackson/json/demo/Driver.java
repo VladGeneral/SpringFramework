332c8ab65f79a489d343f83cc5f03b87d970aaab
Vladislav M <vladgeneralbox@gmail.com>
2020-09-01 15:46:43 +0300

    Section 57: Spring REST read Address and language

--- jackson-databind-json-demo/src/main/java/com/jackson/json/demo/Driver.java
+++ jackson-databind-json-demo/src/main/java/com/jackson/json/demo/Driver.java
@@ -3,6 +3,7 @@ package com.jackson.json.demo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class Driver {
     public static void main(String[] args) {
@@ -17,6 +18,15 @@ public class Driver {
             //print first last name
             System.out.println(student.getFirstName());
             System.out.println(student.getLastName());
+
+            //print adress
+            Address address = student.getAddress();
+            System.out.println("street= " + address.getStreet());
+            System.out.println("city= " + address.getCity());
+
+            //print languages
+            System.out.println("languages: ");
+            Arrays.stream(student.getLanguages()).forEach(System.out::println);
 
         } catch (Exception e){
             e.printStackTrace();
