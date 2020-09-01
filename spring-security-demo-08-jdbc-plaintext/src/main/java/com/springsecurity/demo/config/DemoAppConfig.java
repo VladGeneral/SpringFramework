1e5ae72918a51dee4f2cbe2dbbd185d15febdb20
Vladislav M <vladgeneralbox@gmail.com>
2020-09-01 12:05:56 +0300

    Section 54: Spring Security add properties file

--- spring-security-demo-08-jdbc-plaintext/src/main/java/com/springsecurity/demo/config/DemoAppConfig.java
+++ spring-security-demo-08-jdbc-plaintext/src/main/java/com/springsecurity/demo/config/DemoAppConfig.java
@@ -1,16 +1,29 @@
 package com.springsecurity.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.util.logging.Logger;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.springsecurity.demo")
+@PropertySource("classpath:persistence-postgre.properties")
 public class DemoAppConfig {
+    //set up variable to hold data  from the properties
+    @Autowired
+    private Environment environment;
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
 
     //define bean for ViewResolver
 
@@ -23,5 +36,17 @@ public class DemoAppConfig {
         return viewResolver;
     }
 
-    //
+    //define a bean for our security datasource
+    @Bean
+    public DataSource securityDataSource(){
+        //create connection pool
+
+        //set the jdbc driver class
+
+        //log the connection properties
+
+        //set database connection props
+
+        //set connection pool props
+    }
 }
