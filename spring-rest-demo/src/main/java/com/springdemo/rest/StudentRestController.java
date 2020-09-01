2bc4c1bdcef875f846486e0a3b663b8c8ee04d86
Vladislav M <vladgeneralbox@gmail.com>
2020-09-01 18:08:11 +0300

    Section 60: Spring REST  @PostConstruct fill Arraylist

--- spring-rest-demo/src/main/java/com/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/springdemo/rest/StudentRestController.java
@@ -5,20 +5,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    //define endpoint for "/students" - return list students
+    private List<Student> students;
 
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> students = new ArrayList<>();
+    //define @PostConstruct to load data only once
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
         students.add(new Student("Poornima", "Patel"));
         students.add(new Student("Mario", "Rossi"));
         students.add(new Student("Mary", "Smith"));
+    }
+    //define endpoint for "/students" - return list students
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
         return students;
     }
 }
