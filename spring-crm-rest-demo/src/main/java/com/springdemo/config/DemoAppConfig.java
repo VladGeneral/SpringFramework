5332ad69cbf1806a4b72f2dfc3fb08825ea1b5e9
Vladislav M <vladgeneralbox@gmail.com>
2020-09-02 12:17:00 +0300

    Section 63: Spring REST - UP - no xml configuration

--- spring-crm-rest-demo/src/main/java/com/springdemo/config/DemoAppConfig.java
+++ spring-crm-rest-demo/src/main/java/com/springdemo/config/DemoAppConfig.java
@@ -23,97 +23,96 @@ import java.util.logging.Logger;
 @EnableWebMvc
 @EnableTransactionManagement
 @ComponentScan("com.springdemo")
-@PropertySource({ "classpath:persistence-postgre.properties" })
+@PropertySource({"classpath:persistence-postgre.properties"})
 public class DemoAppConfig implements WebMvcConfigurer {
 
-	@Autowired
-	private Environment env;
-	
-	private Logger logger = Logger.getLogger(getClass().getName());
+    @Autowired
+    private Environment env;
+
+    private Logger logger = Logger.getLogger(getClass().getName());
 
 
-	@Bean
-	public DataSource myDataSource() {
-		
-		// create connection pool
-		ComboPooledDataSource myDataSource = new ComboPooledDataSource();
+    @Bean
+    public DataSource myDataSource() {
 
-		// set the jdbc driver
-		try {
-			myDataSource.setDriverClass(env.getProperty("jdbc.driver"));
-		}
-		catch (PropertyVetoException exc) {
-			throw new RuntimeException(exc);
-		}
-		
-		// for sanity's sake, let's log url and user ... just to make sure we are reading the data
-		logger.info("jdbc.url=" + env.getProperty("jdbc.url"));
-		logger.info("jdbc.user=" + env.getProperty("jdbc.user"));
-		
-		// set database connection props
-		myDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
-		myDataSource.setUser(env.getProperty("jdbc.user"));
-		myDataSource.setPassword(env.getProperty("jdbc.password"));
-		
-		// set connection pool props
-		myDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
-		myDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
-		myDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));		
-		myDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+        // create connection pool
+        ComboPooledDataSource myDataSource = new ComboPooledDataSource();
 
-		return myDataSource;
-	}
-	
-	private Properties getHibernateProperties() {
+        // set the jdbc driver
+        try {
+            myDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException exc) {
+            throw new RuntimeException(exc);
+        }
 
-		// set hibernate properties
-		Properties props = new Properties();
+        // for sanity's sake, let's log url and user ... just to make sure we are reading the data
+        logger.info("jdbc.url=" + env.getProperty("jdbc.url"));
+        logger.info("jdbc.user=" + env.getProperty("jdbc.user"));
 
-		props.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
-		props.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
-		
-		return props;				
-	}
+        // set database connection props
+        myDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        myDataSource.setUser(env.getProperty("jdbc.user"));
+        myDataSource.setPassword(env.getProperty("jdbc.password"));
 
-	
-	// need a helper method 
-	// read environment property and convert to int
-	
-	private int getIntProperty(String propName) {
-		
-		String propVal = env.getProperty(propName);
-		
-		// now convert to int
-		int intPropVal = Integer.parseInt(propVal);
-		
-		return intPropVal;
-	}	
-	
-	@Bean
-	public LocalSessionFactoryBean sessionFactory(){
-		
-		// create session factorys
-		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-		
-		// set the properties
-		sessionFactory.setDataSource(myDataSource());
-		sessionFactory.setPackagesToScan(env.getProperty("hibernate.packagesToScan"));
-		sessionFactory.setHibernateProperties(getHibernateProperties());
-		
-		return sessionFactory;
-	}
-	
-	@Bean
-	@Autowired
-	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
-		
-		// setup transaction manager based on session factory
-		HibernateTransactionManager txManager = new HibernateTransactionManager();
-		txManager.setSessionFactory(sessionFactory);
+        // set connection pool props
+        myDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        myDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        myDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        myDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
 
-		return txManager;
-	}	
-	
+        return myDataSource;
+    }
+
+    private Properties getHibernateProperties() {
+
+        // set hibernate properties
+        Properties props = new Properties();
+
+        props.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        props.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+
+        return props;
+    }
+
+
+    // need a helper method
+    // read environment property and convert to int
+
+    private int getIntProperty(String propName) {
+
+        String propVal = env.getProperty(propName);
+
+        // now convert to int
+        int intPropVal = Integer.parseInt(propVal);
+
+        return intPropVal;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+
+        // create session factorys
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+
+        // set the properties
+        sessionFactory.setDataSource(myDataSource());
+        sessionFactory.setPackagesToScan(env.getProperty("hibernate.packagesToScan"));
+        sessionFactory.setHibernateProperties(getHibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+
+        // setup transaction manager based on session factory
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(sessionFactory);
+
+        return txManager;
+    }
+
 }
 
 
