076021735c0803b345c20fd92c3d33ee27ca09fd
Vladislav M <vladgeneralbox@gmail.com>
2020-09-02 12:40:34 +0300

    Section 64: Spring REST - get customer/s

--- spring-crm-rest-demo/src/main/java/com/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import com.springdemo.entity.Customer;
 import com.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,11 @@ public class CustomerRestController {
     @GetMapping("/customers")
     public List<Customer> getCustomers(){
         return customerService.getCustomers();
+    }
+
+    @GetMapping("/customers/{customerId}")
+    public Customer getCustomers(@PathVariable int customerId){
+
+        return customerService.getCustomer(customerId);
     }
 }
--- spring-crm-rest-demo/src/main/webapp/index.jsp
+++ spring-crm-rest-demo/src/main/webapp/index.jsp
@@ -1,5 +1,7 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+<hr>
+<a href="${pageContext.request.contextPath}/api/customers">Get all customers</a>
 </body>
 </html>
