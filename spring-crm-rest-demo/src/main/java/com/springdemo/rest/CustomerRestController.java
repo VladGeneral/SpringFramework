f0b5ebeb06720a79ba5dbecf3c84cb1f55210835
Vladislav M <vladgeneralbox@gmail.com>
2020-09-02 14:03:29 +0300

    Section 64: Spring REST - @PostMapping | @RequestBody - addCUstomer

--- spring-crm-rest-demo/src/main/java/com/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/springdemo/rest/CustomerRestController.java
@@ -3,10 +3,7 @@ package com.springdemo.rest;
 import com.springdemo.entity.Customer;
 import com.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +25,18 @@ public class CustomerRestController {
         if (customer == null){
             throw new CustomerNotFoundException("Customer not found - " + customerId);
         }
+        return customer;
+    }
+
+    //add mapping for Post /customers - add new customer
+    @PostMapping("/customers")
+    public Customer addCustomer(@RequestBody Customer customer){
+        //also just in case the pass an id in JSON set id to 0
+        //this is force a save of new item
+        customer.setId(0);
+
+        customerService.saveCustomer(customer);
+
         return customer;
     }
 }
