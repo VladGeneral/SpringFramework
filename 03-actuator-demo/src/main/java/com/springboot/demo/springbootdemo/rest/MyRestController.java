171ceb97bcd334895e09c577b292e2186b6b4393
Vladislav M <vladgeneralbox@gmail.com>
2020-09-03 14:45:25 +0300

    Section 71: Spring Boot - injecting custom application properties

--- 03-actuator-demo/src/main/java/com/springboot/demo/springbootdemo/rest/MyRestController.java
+++ 03-actuator-demo/src/main/java/com/springboot/demo/springbootdemo/rest/MyRestController.java
@@ -1,5 +1,6 @@
 package com.springboot.demo.springbootdemo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,17 @@ import java.time.LocalDateTime;
 
 @RestController
 public class MyRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return coachName + teamName;
+    }
 
     @GetMapping("/")
     public String sayHello(){
