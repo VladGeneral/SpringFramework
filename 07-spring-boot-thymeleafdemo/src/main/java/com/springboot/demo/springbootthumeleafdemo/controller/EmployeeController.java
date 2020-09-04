0491fd367832572c65ff336c5db7da01a2e9f071
Vladislav M <vladgeneralbox@gmail.com>
2020-09-04 15:12:34 +0300

    Section 79: Spring Boot - Thymeleaf build HTML tables - create Employee class and controller

---
+++ 07-spring-boot-thymeleafdemo/src/main/java/com/springboot/demo/springbootthumeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.springboot.demo.springbootthumeleafdemo.controller;
+
+import com.springboot.demo.springbootthumeleafdemo.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    private List<Employee> employees;
+
+    @PostConstruct
+    private void loadData() {
+        Employee employee1 = new Employee(1, "name1", "lastname1", "ssds@gmailcom");
+        Employee employee2 = new Employee(2, "name2", "lastname2", "ss2@gmailcom");
+        Employee employee3 = new Employee(3, "name3", "lastname3", "ss3@gmailcom");
+
+        employees = new ArrayList<>();
+        employees.add(employee1);
+        employees.add(employee2);
+        employees.add(employee3);
+    }
+}
