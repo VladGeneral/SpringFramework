a41ed07adc4d3d3f056ee2f834c07456479da117
Vladislav M <vladgeneralbox@gmail.com>
2020-09-04 17:07:28 +0300

    Section 80: Spring Boot - Thymeleaf - add "add employee" button -> create save form | save controller

--- 08-spring-boot-thymeleafdemo-db/src/main/java/com/springboot/demo/springbootthymeleafdemodb/controller/EmployeeController.java
+++ 08-spring-boot-thymeleafdemo-db/src/main/java/com/springboot/demo/springbootthymeleafdemodb/controller/EmployeeController.java
@@ -6,11 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("/employees")
@@ -26,6 +24,20 @@ public class EmployeeController {
     @GetMapping("/list")
     public String listEmployees(Model model){
         model.addAttribute("employees", employees.findAll());
-        return "list-employees";
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model){
+        Employee employee = new Employee();
+
+        model.addAttribute("employee", employee);
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        employees.save(employee);
+        return "redirect:/employees/list";
     }
 }
--- 08-spring-boot-thymeleafdemo-db/src/main/java/com/springboot/demo/springbootthymeleafdemodb/dao/EmployeeRepository.java
+++ 08-spring-boot-thymeleafdemo-db/src/main/java/com/springboot/demo/springbootthymeleafdemodb/dao/EmployeeRepository.java
@@ -3,5 +3,7 @@ package com.springboot.demo.springbootthymeleafdemodb.dao;
 import com.springboot.demo.springbootthymeleafdemodb.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 }
