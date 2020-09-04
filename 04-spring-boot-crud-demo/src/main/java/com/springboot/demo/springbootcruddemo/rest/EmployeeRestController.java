94e940be931f06167fa9beaa6ba259250151666c
Vladislav M <vladgeneralbox@gmail.com>
2020-09-04 10:51:57 +0300

    Section 74: Spring Boot - add Service layer

--- 04-spring-boot-crud-demo/src/main/java/com/springboot/demo/springbootcruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-crud-demo/src/main/java/com/springboot/demo/springbootcruddemo/rest/EmployeeRestController.java
@@ -2,6 +2,9 @@ package com.springboot.demo.springbootcruddemo.rest;
 
 import com.springboot.demo.springbootcruddemo.dao.EmployeeDAO;
 import com.springboot.demo.springbootcruddemo.entity.Employee;
+import com.springboot.demo.springbootcruddemo.service.EmployeeService;
+import com.springboot.demo.springbootcruddemo.service.EmployeeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +15,15 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
-    public EmployeeRestController(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    @Autowired
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     @GetMapping("/employees")
     public List<Employee> getEmployees() {
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 }
