5f7e09ec229739747d625997570d48a647449a54
Vladislav M <vladgeneralbox@gmail.com>
2020-09-04 14:13:18 +0300

    Section 77: Spring Boot - Spring Data REST Configuration(@RepositoryRestResource (path = "members")), pagination, sorting(employees/?sort=lastname)

--- 06-spring-boot-data-rest-cruddemo/src/main/java/com/springboot/demo/springbootcruddemo/dao/EmployeeRepository.java
+++ 06-spring-boot-data-rest-cruddemo/src/main/java/com/springboot/demo/springbootcruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,9 @@ package com.springboot.demo.springbootcruddemo.dao;
 
 import com.springboot.demo.springbootcruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-                                                        //8080/employees - default
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//8080/employees - default
+//@RepositoryRestResource (path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 }
