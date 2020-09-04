ba2546f9cebec534ca7f05aa9fb91f681ae7ca53
Vladislav M <vladgeneralbox@gmail.com>
2020-09-04 13:25:11 +0300

    Section 77: Spring Boot - Spring Data REST add properties(path)

--- 06-spring-boot-data-rest-cruddemo/src/main/java/com/springboot/demo/springbootcruddemo/dao/EmployeeRepository.java
+++ 06-spring-boot-data-rest-cruddemo/src/main/java/com/springboot/demo/springbootcruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,6 @@ package com.springboot.demo.springbootcruddemo.dao;
 
 import com.springboot.demo.springbootcruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+                                                        //8080/employees - default
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 }
