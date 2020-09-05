81d62e37ab9648f67a6f907c99b4e99cc4352a87
Vladislav M <vladgeneralbox@gmail.com>
2020-09-05 13:02:20 +0300

    Section 81: Login logout with spring boot and thymeleaf

--- spring-security-user-registration-custom-user-details/src/main/java/com/springsecurity/demo/config/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.springsecurity.demo.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer 
-						extends AbstractSecurityWebApplicationInitializer {
-
-}
