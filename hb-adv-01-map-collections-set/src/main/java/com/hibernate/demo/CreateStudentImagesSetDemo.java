d729ab800af2d8c05b97632e390ecdad0cb7517d
Vladislav M <vladgeneralbox@gmail.com>
2020-09-15 14:08:16 +0300

    Hibernate adv Section 2: mapping sets

---
+++ hb-adv-01-map-collections-set/src/main/java/com/hibernate/demo/CreateStudentImagesSetDemo.java
@@ -0,0 +1,33 @@
+package com.hibernate.demo;
+
+import com.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Set;
+
+public class CreateStudentImagesSetDemo {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+
+        try(Session session = sessionFactory.getCurrentSession()) {
+            Student student = new Student("john", "doe", "johnd@dfs.dd");
+            Set<String> images = student.getImages();
+            images.add("photo1.jpg");
+            images.add("photo2.jpg");
+            images.add("photo3.jpg");
+            images.add("photo4.jpg");
+
+            session.beginTransaction();
+
+            session.persist(student);
+
+            session.getTransaction().commit();
+        }
+    }
+}
