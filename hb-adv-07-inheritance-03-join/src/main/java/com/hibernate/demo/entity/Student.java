6686dd8203d90d9ee107e80a1af1563e44d81700
Vladislav M <vladgeneralbox@gmail.com>
2020-09-16 18:48:21 +0300

    Hibernate adv Section 8: Inheritance Mapping - Joined Table Strategy/ set up project

---
+++ hb-adv-07-inheritance-03-join/src/main/java/com/hibernate/demo/entity/Student.java
@@ -0,0 +1,26 @@
+package com.hibernate.demo.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Student extends User{
+
+    private String course;
+
+    public Student(String firstName, String lastName, String email, String course) {
+        super(firstName, lastName, email);
+        this.course = course;
+    }
+
+    public Student() {
+
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+}
