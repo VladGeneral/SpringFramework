701da784c570cf2f6b308d530d37fb04e38c1337
Vladislav M <vladgeneralbox@gmail.com>
2020-09-16 15:45:47 +0300

    Hibernate adv Section 5: Mapping Enums - @Enumerated(EnumType.STRING) for enum class

---
+++ hb-adv-06-map-enum/src/main/java/com/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,37 @@
+package com.hibernate.demo;
+
+import com.hibernate.demo.entity.Address;
+import com.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+import java.util.Set;
+
+public class CreateStudentDemo {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .addAnnotatedClass(Address.class)
+                .buildSessionFactory();
+
+
+        try(Session session = sessionFactory.getCurrentSession()) {
+            Student student = new Student("john", "doe", "johnd@dfs.dd");
+
+            Address billingAddress = new Address("some bstreet","some bsity", "60b0222");
+
+            session.beginTransaction();
+
+
+            student.setBillingAddress(billingAddress);
+
+
+            session.persist(student);
+
+            session.getTransaction().commit();
+        }
+    }
+}
