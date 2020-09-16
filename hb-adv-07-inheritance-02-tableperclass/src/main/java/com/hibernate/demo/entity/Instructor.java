d5c1f09d9900db927f05193cf19bee1f13f3c84c
Vladislav M <vladgeneralbox@gmail.com>
2020-09-16 18:31:32 +0300

    Hibernate adv Section 7: Inheritance mapping - Table per Class strategy - set up project

---
+++ hb-adv-07-inheritance-02-tableperclass/src/main/java/com/hibernate/demo/entity/Instructor.java
@@ -0,0 +1,27 @@
+package com.hibernate.demo.entity;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue(value = "INSTRUCTOR")//any value here
+public class Instructor extends User {
+    private Double salary;
+
+    public Instructor(String firstName, String lastName, String email, Double salary) {
+        super(firstName, lastName, email);
+        this.salary = salary;
+    }
+
+    public Instructor() {
+
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+}
