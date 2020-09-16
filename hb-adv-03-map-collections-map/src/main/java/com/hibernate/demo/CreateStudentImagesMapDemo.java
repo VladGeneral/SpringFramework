5d4406b5796f639bef18386f86f7fd3463bfa45d
Vladislav M <vladgeneralbox@gmail.com>
2020-09-16 11:47:31 +0300

    Hibernate adv Section 3: Mapping collections - Maps

---
+++ hb-adv-03-map-collections-map/src/main/java/com/hibernate/demo/CreateStudentImagesMapDemo.java
@@ -0,0 +1,35 @@
+package com.hibernate.demo;
+
+import com.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class CreateStudentImagesMapDemo {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+
+        try(Session session = sessionFactory.getCurrentSession()) {
+            Student student = new Student("john", "doe", "johnd@dfs.dd");
+            Map<String, String> images = student.getImages();
+            images.put("photo1.jpg","photo1");
+            images.put("photo2.jpg","photo2");
+            images.put("photo3.jpg","photo3");
+            images.put("photo4.jpg","photo4");
+
+            session.beginTransaction();
+
+            session.persist(student);
+
+            session.getTransaction().commit();
+        }
+    }
+}
